42f1ba831db82451a8b7327890c6b3f03ef215b3
Russell <34661910+russelloverton@users.noreply.github.com>
2025-09-05 10:06:01 -0500

    added bike file

---
+++ src/ica/01-python+ide/bike.py
@@ -0,0 +1,5 @@
+trip_dist = 420    # total miles in the trip
+bike_per_day = 50   # miles you can bicycle in a day
+trip_days = trip_dist / bike_per_day
+print("To bike", trip_dist, "miles, ", bike_per_day, "miles per day,")
+print(" takes", trip_days, "days.")
