d39c9669d4e62032bea1f38fe7e705378d041e77
Russell <34661910+russelloverton@users.noreply.github.com>
2025-09-08 10:28:58 -0500

    aunt judy

---
+++ src/ica/02-data_types/test_file.py
@@ -0,0 +1,25 @@
+'Hi there'
+"Hi there"
+"I contain an apostrophe, don't I?"
+'I was told, "Double quotes go inside single quotes!"'
+s = 'hi mom'
+longstr = """As I was going to St. Ives,
+I met a man with seven wives.
+Each wife had seven sacks,
+Each sack had seven cats,
+Each cat had seven kits.
+Kits, cats, sacks, and wives:
+How many were going to St. Ives?"""
+print(s)
+print(longstr)
+
+print(len('foo'))
+print('foo' + 'bar')
+print('foo' * 3)
+print('mom' in s)
+print(s[3])
+print(s[3:5])
+
+print(len(longstr))
+print('kits' in longstr)
+print(longstr[36:39])
