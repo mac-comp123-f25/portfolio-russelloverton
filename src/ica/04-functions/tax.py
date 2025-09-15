c9c19b917e433bd977adb80ff08118e63611c38f
Russell <34661910+russelloverton@users.noreply.github.com>
2025-09-15 10:17:09 -0500

    difference.py changed

---
+++ src/ica/04-functions/tax.py
@@ -0,0 +1,13 @@
+def add_tax(price, tax_rate):
+    tax_amt = price * tax_rate
+    return price + tax_amt
+
+
+add_tax(25.99, 0.0725)
+
+
+#Local environment
+#price  25.99
+#tax_rate  .0725
+#tax_amt   1.884
+#<so on>
