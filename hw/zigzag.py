05d99d78d61bf187d249ef6ac2a114843bff3843
Russell <34661910+russelloverton@users.noreply.github.com>
2025-09-17 09:46:06 -0500

    changed

--- hw/zigzag.py
+++ hw/zigzag.py
@@ -1,4 +1,5 @@
 import turtle
+from random import randint
 
 first_turtle = turtle.Turtle()
 second_turtle = turtle.Turtle()
@@ -11,21 +12,31 @@ third_turtle.color("blue")
 first_turtle.speed(0)
 second_turtle.speed(0)
 third_turtle.speed(0)
+turtle.colormode(255)
+
 
 first_turtle.penup()
 second_turtle.penup()
 third_turtle.penup()
-first_turtle.setx(-300)
-second_turtle.setx(-300)
-third_turtle.setx(-300)
-first_turtle.sety(280)
+first_turtle.setx(-350)
+second_turtle.setx(-350)
+third_turtle.setx(-350)
+first_turtle.sety(250)
 second_turtle.sety(300)
-third_turtle.sety(320)
+third_turtle.sety(350)
 first_turtle.pendown()
 second_turtle.pendown()
 third_turtle.pendown()
+first_turtle.width(10)
+second_turtle.width(10)
+third_turtle.width(10)
+
 
 for _ in range(100):
+    turtle.bgcolor(randint(0, 255), randint(0, 255), randint(0, 255))
+    first_turtle.color(randint(0, 255), randint(0, 255), randint(0, 255))
+    second_turtle.color(randint(0, 255), randint(0, 255), randint(0, 255))
+    third_turtle.color(randint(0, 255), randint(0, 255), randint(0, 255))
     first_turtle.forward(10)
     second_turtle.forward(10)
     third_turtle.forward(10)
