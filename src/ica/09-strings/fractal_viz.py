7bea2d9a3e29f2a8f3781c4bdb8baf1d85b7d70c
Russell <34661910+russelloverton@users.noreply.github.com>
2025-10-06 10:30:47 -0500

    made some bad fractals

--- src/ica/09-strings/fractal_viz.py
+++ src/ica/09-strings/fractal_viz.py
@@ -51,6 +51,22 @@ def draw_combination_of_lacks_and_islands_curve(tur, n, angle, step_size):
 
     draw_fractal(tur, l_system, angle, step_size)
 
+def weird_thing(tur, n, angle, step_size):
+    axiom = 'F+F+F+F'
+    rules = {'F': 'F+fff-FF-FFF+RFFFF+f',
+             'f': 'ffffff'}
+    l_system = apply_l_system(axiom, rules, n)
+
+    draw_fractal(tur, l_system, angle, step_size)
+
+def weird_thing_2(tur, n, angle, step_size):
+    axiom = 'F+F+F+F'
+    rules = {'F': 'F+fff-FF-F-F-F-F-FFF+f',
+             'f': 'f-ff+ff+f+LLLLLL+ff-f'}
+    l_system = apply_l_system(axiom, rules, n)
+
+    draw_fractal(tur, l_system, angle, step_size)
+
 
 if __name__ == '__main__':
     scr = turtle.Screen()
@@ -60,11 +76,13 @@ if __name__ == '__main__':
     tur.up()
     tur.down()
 
-    draw_koch_curve(tur, 4, 60, 5)
+    # draw_koch_curve(tur, 4, 60, 5)
     # draw_hilbert_curve(tur, 4, 90, 5)
     # draw_quadratic_koch_islad(tur, 3, 90, 5)
     # draw_dragon_curve(tur, 3, 90, 5)
     # draw_combination_of_lacks_and_islands_curve(tur, 2, 90, 5)
+    # weird_thing(tur, 2, 90, 5)
+    weird_thing_2(tur, 2, 90, 5)
 
     tur.up()
     tur.forward(50)
